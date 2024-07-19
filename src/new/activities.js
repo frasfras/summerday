fdb0b57438f2d820efe6f4eb2afedbe0ce074d65
frasfras <65541080+frasfras@users.noreply.github.com>
2024-07-19 10:48:33 +0800

    Add files via upload

---
+++ src/new/activities.js
@@ -0,0 +1,9 @@
+export const activities = [
+  { id: "swim", name: "swim in the cool lake", energyCost: { min: 10, max: 30 }, funGain: { min: 15, max: 25 } },
+  { id: "barbecue", name: "have a backyard barbecue", energyCost: { min: 10, max: 30 }, funGain: { min: 15, max: 25 } },
+  { id: "bike", name: "go on a bike ride", energyCost: { min: 10, max: 30 }, funGain: { min: 15, max: 25 } },
+  { id: "frisbee", name: "play frisbee in the park", energyCost: { min: 10, max: 30 }, funGain: { min: 15, max: 25 } },
+  { id: "iceCream", name: "make homemade ice cream", energyCost: { min: 10, max: 30 }, funGain: { min: 15, max: 25 } },
+  { id: "sunset", name: "watch the sunset at the beach", energyCost: { min: 10, max: 30 }, funGain: { min: 15, max: 25 } },
+  { id: "stargaze", name: "stargaze on a warm night", energyCost: { min: 10, max: 30 }, funGain: { min: 15, max: 25 } },
+];
